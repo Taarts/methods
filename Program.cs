b223b3e5c2a68de7747e0e6e91d35717bd1c34e8
Tricia Amheiser <tricia@taarts.net>
2022-05-02 22:36:39 -0400

    Add flair & 'try.parse' eliminates non-int input

--- Program.cs
+++ Program.cs
@@ -12,22 +12,57 @@ namespace EmployeeDatabase
             Console.WriteLine("--------------------------------");
             Console.WriteLine();
             Console.WriteLine();
+        } /* take input ----> */
+        static string PromptForString(string prompt) /* this is a method */
+        {
+            Console.WriteLine("💙💚💛🧡❤️💜");
+            // Use the argument, whatever the caller sent us.
+            Console.Write(prompt);
+
+            // Get some user input
+            var userInput = Console.ReadLine();
+
+            // RETURN that value as the output of this method.
+            // The value in `userInput` will go wherever the
+            // *CALLER* of the method has specified.
+            return userInput;
         }
+        static int PromptForInteger(string prompt) /* this has to be a string prompt for the console output */
+        {
+            //Console.Write(prompt);<- exchanged below for this 
+
+            var userInput = PromptForString(prompt);
+
+            int inputAsInteger;
+            var isThisGoodInput = int.TryParse(userInput, out inputAsInteger);
+            // var inputAsInteger = int.Parse(userInput);
+
+            if (isThisGoodInput)
+            {
+                return inputAsInteger;
+            }
+            else
+            {
+                Console.WriteLine("uh-oh, that's not a number value - have a 0.");
+                return 0;
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
             // 1st method called on 
             DisplayGreeting();
 
-            Console.Write("What is your name? ");
-            var name = Console.ReadLine();
+            var name = PromptForString("What is your name? "); /* ----> return output */
 
-            Console.Write("What is your department number? ");
-            var department = int.Parse(Console.ReadLine());
+            var department = PromptForInteger("What is your department number? ");
 
-            Console.Write("What is your yearly salary (in dollars)? ");
-            var salary = int.Parse(Console.ReadLine());
+            var salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+            //var salary = int.Parse(Console.ReadLine());
 
-            var salaryPerMonth = salary / 12;
+            var salaryPerMonth = salary / 12.0;
             Console.WriteLine($"Hello, {name} you make {salaryPerMonth} a month.");
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # methods
+
+1st method
+
+removing "fluff" from your code.
+Don't Repeat Yourself - DRY
+
+Conventions - Pascal Case
+
+For the greeting - no additional input needed from 'outside'.
+void refers to nothing (like Space, the final frontier)
+
+When we call the method we have to include parenthesis even if, as in this case, there aren't any arguments.
+The way you read the line is "Call the DisplayGreeting method, providing no arguments, and expecting no return."
+
+/_ take input ----> _/
+Use the argument, whatever the caller sent us.
+Get some user input
+RETURN that value as the output of this method.
+The value in `userInput` will go wherever the
+_CALLER_ of the method has specified.
+return userInput;
